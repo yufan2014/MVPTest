f98a2e3d7da3f42661e336cec1db8c2aa8e73140
weiyu2016 <handongyu2013@126.com>
2016-11-04 16:07:07 +0800

    mvp暂时完善

--- app/src/main/java/com/mvptest/http/retrofit/ApiClient.java
+++ app/src/main/java/com/mvptest/http/retrofit/ApiClient.java
@@ -2,9 +2,13 @@ package com.mvptest.http.retrofit;
 
 import com.mvptest.BuildConfig;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -23,7 +27,25 @@ public class ApiClient {
         if(retrofit == null) {
             OkHttpClient.Builder builder = new OkHttpClient.Builder()
                     .readTimeout(10000, TimeUnit.MILLISECONDS)
-                    .connectTimeout(10000, TimeUnit.MILLISECONDS);
+                    .connectTimeout(10000, TimeUnit.MILLISECONDS)
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request request = chain.request()
+                                    .newBuilder()
+                                    .addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+                                    .addHeader("Accept-Encoding", "gzip, deflate")
+                                    .addHeader("Connection", "keep-alive")
+                                    .addHeader("Accept", "*/*")
+                                    .addHeader("Cookie", "add cookies here")
+                                    .addHeader("uid", "add uid here")
+                                    .addHeader("token", "add token here")
+                                    .addHeader("version", "add version here")
+                                    .addHeader("buildCode", "add buildCode here")
+                                    .build();
+                            return chain.proceed(request);
+                        }
+                    });
 
             if (BuildConfig.DEBUG) {
                 // Log信息拦截器
